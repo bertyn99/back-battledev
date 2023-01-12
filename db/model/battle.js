8b4a575a04bf6a4264c0e88729d3feab2e7328af
Bertyn Boulikou <bboulikou@gmail.com>
2023-01-12 11:18:04 +0100

    fix populate data

--- db/model/battle.js
+++ db/model/battle.js
@@ -2,18 +2,21 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 let battleSchema = new Schema(
   {
-    idOpponents: [{ type: Schema.Types.ObjectId, ref: "user" }],
+    idOpponents: [{ type: Schema.Types.ObjectId, ref: "User" }],
     category: { type: String },
     quizzpoints: [{ type: Number }],
-    winner:{ type: Schema.Types.ObjectId, ref: "user" }
+    winner: { type: Schema.Types.ObjectId, ref: "User" },
   },
   { timestamps: true }
 );
 
-battleSchema.statics.findBattlesOfthisUser = async function(idUser){
-  const b=await Battle.find({ 'idOpponents': idUser  })
+battleSchema.statics.findBattlesOfthisUser = async function (idUser) {
+  const b = await Battle.find({ idOpponents: idUser }).populate(
+    "idOpponents winner",
+    "username"
+  );
   return b;
-}
-battleSchema.set('toJSON', { getters: true, virtuals: false });
+};
+battleSchema.set("toJSON", { getters: true, virtuals: false });
 const Battle = mongoose.model("Battle", battleSchema);
 export default Battle;
